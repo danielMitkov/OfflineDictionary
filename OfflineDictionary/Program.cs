83f280f990267f61b1ff1cf654d0b8560c6391ea
danielMitkov <daniel.mitkov01@gmail.com>
2025-11-18 18:49:34 +0200

    Showing more words on the same screen

--- OfflineDictionary/Program.cs
+++ OfflineDictionary/Program.cs
@@ -5,16 +5,77 @@ internal class Program
     static void Main(string[] args)
     {
         string[] words = GetWords.Run("OfflineDictionary.words.txt", 370105);
-        string input = string.Empty;
+
         while (true)
         {
-            Console.Clear();
-            for (int i = 0; i < words.Length && input != string.Empty; ++i)
-            {
-                if (words[i].StartsWith(input)) Console.WriteLine(words[i]);
-            }
             Console.WriteLine(Environment.NewLine + "Enter The Beginning Of A Word:");
-            input = Console.ReadLine()!;
+
+            string input = Console.ReadLine()!;
+            if (input == string.Empty) continue;
+            input = input.ToLower();
+
+            List<string> results = new();
+
+            for (int i = 0; i < words.Length; ++i)
+            {
+                bool isMatch = IsMatching(words[i], input);
+
+                if (isMatch) results.Add(words[i]);
+            }
+
+            List<List<string>> columnsResults = new();
+            columnsResults.Add(new List<string>());
+            columnsResults.Add(new List<string>());
+            columnsResults.Add(new List<string>());
+            columnsResults.Add(new List<string>());
+            columnsResults.Add(new List<string>());
+
+            int resultI = 0;
+            int columnSize = (int)Math.Ceiling(results.Count / (decimal)columnsResults.Count);
+
+            for (int listI = 0; listI < columnsResults.Count; ++listI)
+            {
+                for (int count = columnSize; count > 0; --count)
+                {
+                    if (resultI >= results.Count) break;
+
+                    columnsResults[listI].Add(results[resultI++]);
+                }
+            }
+
+            Console.WriteLine($"Results For \"{input}\":");
+
+            for (int columnI = 0; columnI < columnSize; ++columnI)
+            {
+                for (int listI = 0; listI < columnsResults.Count; ++listI)
+                {
+                    if (columnI >= columnsResults[listI].Count) break;
+
+                    int spacesCount = 32;// max length word + 1
+                    spacesCount -= columnsResults[listI][columnI].Length;
+
+                    string spaces = new string(Enumerable.Repeat(' ', spacesCount).ToArray());
+
+                    Console.Write($"{columnsResults[listI][columnI]}{spaces}");
+                }
+                Console.WriteLine();
+            }
         }
+    }
+
+    private static bool IsMatching(string word, string target)
+    {
+        bool isMatch = true;
+
+        for (int c = 0; c < target.Length; ++c)
+        {
+            if (word.Length < target.Length || word[c] != target[c])
+            {
+                isMatch = false;
+                break;
+            }
+        }
+
+        return isMatch;
     }
 }
